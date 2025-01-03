52488489b7e2d8dd8f578bdef44d4556ba8754b4
Tariq Ahmad Abu Alsheikh <158696128+Tariq-Ahmad-99@users.noreply.github.com>
2025-01-03 10:03:46 +0300

    prepare the data for the model

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
+import numpy as np
 import yfinance as yf
-import pandas as pd 
-import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential # type: ignore
+from keras.layers import Dense, LSTM # type: ignore
 plt.style.use('fivethirtyeight')
 
 # Create the variables and importing the data
@@ -11,16 +14,30 @@ start = datetime(end.year-15, end.month, end.day)
 stock = 'BTC-USD'
 stock_data = yf.download(stock, start=start, end=end)
 
-
 #Close price data handling
 close_prices = stock_data['Close']
 
-# Plot Closing price with encasement
-plt.figure(figsize=(15, 6))
-plt.plot(close_prices.index, close_prices, label='Close Price', color='Blue', linewidth=1)
-plt.title('Close Price of Bitcoin over time', fontsize=16)
-plt.xlabel('Years', fontsize=14)
-plt.ylabel('Close Price', fontsize=14)
-plt.grid(alpha=0.3)
-plt.legend(fontsize=12)
-#plt.show()
+
+## Building the LSTM model
+
+# Scale the data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(close_prices.dropna())
+
+#Prepare data for the model
+x_data = []
+y_data = []
+base_days = 100
+for i in range(base_days, len(scaled_data)):
+    x_data.append(scaled_data[i-base_days:i])
+    y_data.append(scaled_data[i])
+
+x_data = np.array(x_data)
+x_data = np.array(y_data)
+
+#Split the data to train and test
+train_size = int(len(x_data) * 0.9)
+x_train, y_train = x_data[:train_size], y_data[:train_size]
+x_test, y_test = x_data[train_size:], y_data[train_size:]
+
+#Creating the model
